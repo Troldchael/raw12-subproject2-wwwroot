92464d8a57e23a74a4d06911fb3ecfc337ad8c49
Nicooo <43990037+Troldchael@users.noreply.github.com>
2020-11-19 10:37:51 +0100

    included web libraries

---
+++ WebService/js/Data.js
@@ -0,0 +1,37 @@
+﻿console.log("before fetch");
+
+let getCategories = function(callback) {
+
+    fetch("api/categories")
+        .then(function(response) {
+
+            return response.json();
+        })
+        .then(function(data) {
+            callback(data);
+
+        });
+    
+};
+
+let createCategory = function(category, callback) {
+
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    fetch("api/categories", { method: "POST", body: JSON.stringify(category), headers })
+        .then(response => response.json())
+        .then(data => callback(data));
+};
+
+getCategories(function(data) {
+    console.log(data);
+});
+
+/*createCategory({Name: "ffsf", Description: "sfsfsf"},
+
+    function(data) {
+        console.log(data);
+
+});*/
+
+console.log("after fetch!");
