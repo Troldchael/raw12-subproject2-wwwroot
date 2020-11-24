3645c066417a83ec58b9efbda3894d095e61d56d
Troldchael <nianmo@ruc.dk>
2020-11-24 16:28:25 +0100

    added some parseJson. maybe it will work

--- WebService/wwwroot/js/actorModel.js
+++ WebService/wwwroot/js/actorModel.js
@@ -4,12 +4,10 @@
 
     //let actorArray = getActors(data);
 
-    let actors = ko.observableArray(
-        []
-    );
+    let actors = ko.observableArray([]);
 
-    let primaryName = ko.observable();
-    let nConst = ko.observable();
+    /*let primaryName = ko.observable();
+    let nConst = ko.observable();*/
 
     fetch("api/actors")
         .then(function (response) {
@@ -18,6 +16,16 @@
         .then(function (data) {
             actors(data);
         });
+
+    // Load and parse the JSON
+    var someJSON = actors/* Omitted: fetch it from the server however you want */;
+    var parsed = JSON.parse(someJSON);
+
+    // Update view model properties
+    actorModel.firstName(parsed.PrimaryName);
+    actorModel.pets(parsed.NConst);
+
+    // hint for Json https://knockoutjs.com/documentation/json-data.html
 
     console.log(actors);
 
