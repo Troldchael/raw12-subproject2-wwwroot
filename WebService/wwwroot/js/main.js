ddff5ac3e75ed84728a85f0e5b092371d2415384
Nicooo <43990037+Troldchael@users.noreply.github.com>
2020-12-01 13:22:30 +0100

    trying henriks paging example for frontend data

--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -1,13 +1,25 @@
-﻿/// <reference path="lib/knockout/knockout-latest.js" />
-
-require.config({
+﻿require.config({
     baseUrl: "js",
     paths: {
-        knockout: "lib/knockout/knockout-latest.debug"
+        knockout: "lib/knockout/knockout-latest.debug",
+        text: "lib/require-text/text.min",
+        dataservice: "services/dataService"
     }
 });
 
-require(['knockout', 'actorModel'], function (ko, vm) {
-    console.log(vm);
+require(['knockout', 'text'], (ko) => {
+
+    ko.components.register('product-list', {
+        viewModel: { require: "components/product/productList"},
+        template: { require: "text!components/product/productList.html" }
+
+    });
+
+
+});
+
+
+require(['knockout', 'viewModel'], (ko, vm) => {
+
     ko.applyBindings(vm);
 });
