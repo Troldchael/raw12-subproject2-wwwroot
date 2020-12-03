06549ac1e47b195a0edd132a7079c37143a2ae0e
ChilliMads <55391223+ChilliMads@users.noreply.github.com>
2020-12-03 13:25:17 +0100

    Bootstrap

--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -1,1 +1,34 @@
-﻿
+﻿/// <reference path="lib/twitter-bootstrap/js/bootstrap.bundle.min" />
+/// <reference path="lib/require.js/require.min.js" />
+/// <reference path="lib/jquery/jquery.min.js" />
+require.config({
+    baseUrl: "js",
+    paths: {
+        knockout: "lib/knockout/knockout-latest.debug",
+        text: "lib/require-text/text.min",
+        jquery: "lib/jquery/jquery.min",
+        bootstrap: "lib/twitter-bootstrap/js/bootstrap.bundle.min",
+        postman: "lib/services/postman"
+    },
+    shim: {
+        bootstrap: ['jquery']
+    }
+});
+
+require(['knockout', 'text'], (ko) => {
+    ko.components.register('home', {
+        viewModel: { require: "components/home/home" },
+        template: { require: "text!components/home/home.html" }
+    });
+
+    ko.components.register('contact', {
+        viewModel: { require: "components/contact/contact" },
+        template: { require: "text!components/contact/contact.html" }
+    });
+});
+
+
+
+require(['knockout', 'viewModel', 'bootstrap'], (ko, vm) => {
+    ko.applyBindings(vm);
+});
