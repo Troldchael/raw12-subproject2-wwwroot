f4af1c0e92b13a6d6358c3085b9f19d16ef07674
Troldchael <nianmo@ruc.dk>
2020-12-14 19:31:57 +0100

    added movie-compare to viewmodel

--- WebService/wwwroot/js/viewModel.js
+++ WebService/wwwroot/js/viewModel.js
@@ -1,18 +1,14 @@
 ﻿define(['knockout', 'postman'], (ko, postman) => {
 
-    //let currentComponent = ko.observable("browse-actors");
-
-    
     let menuElements = [
         { name: "Home", component: "home" },
         { name: "User", component: "user" },
         { name: "Actors", component: "browse-actors" },
         { name: "Movies", component: "browse-movies" },       
-        { name: "Compare", component: "" },
+        { name: "Compare", component: "movie-compare" },
         { name: "Login", component: "login" }
-
-
     ];
+
     let currentComponent = ko.observable(menuElements[0].component);
     let changeContent = element => {
 
